0b48117667ae6703c844c1c57bd87c996e54182a
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-19 14:56:49 +0300

    updated sidebar buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 interface ButtonProps {
     children: any,
-    onClick: () => void
+    onClick: () => void,
+    selected: boolean,
+    props?: any
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, onClick = () => { } }) => {
+export const Button: React.FC<ButtonProps> = ({ children, onClick = () => { }, selected = false, ...props }) => {
     return (
-        <button onClick={onClick} className="w-full px-4 py-2 bg-gray-100 hover:bg-gray-300 transition-colors rounded-lg">
+        <button onClick={onClick} className={`w-full px-4 py-1 capitalize ${selected ? "text-lightBlue-500" : "text-gray-600"} bg-white hover:bg-gray-100 transition-colors rounded-lg text-left`}>
             {children}
         </button>
     )
--- src/containers/CoffeeBook/modules/Sidebar.tsx
+++ src/containers/CoffeeBook/modules/Sidebar.tsx
@@ -6,14 +6,28 @@ import { $set_filter, $set_search } from '../../../redux/coffee.reducer'
 
 export const Sidebar: React.FC = () => {
     const $coffee = useSelector(state => state.$coffee)
-    console.log($coffee);
-
     const dispatch = useDispatch()
     return (
         <aside className="col-span-4 border-b md:border-r md:border-b-0 border-gray-300 pt-8 pb-2 h-auto md:h-screen sticky top-0 left-0 bg-white">
             <SearchBar value={$coffee?.search || ""} onChange={(srch) => dispatch($set_search(srch))} placeholder="Search" />
-            <div className="flex flex-col space-y-2 mt-4 px-2"><Button onClick={() => dispatch($set_filter(""))}>All Coffees</Button>
-                {$coffee.categories.map(category => <Button onClick={() => dispatch($set_filter(category))}>{category}</Button>)}</div>
+            <div className="flex flex-col space-y-2 mt-4 px-2">
+                <Button
+                    selected={$coffee.filter === ""}
+                    onClick={() => dispatch($set_filter(""))}
+                >
+                    All Coffees
+                </Button>
+                {$coffee.categories
+                    .map(category =>
+                        <Button
+                            selected={$coffee.filter === category}
+                            onClick={() => dispatch($set_filter(category))}
+                        >
+                            {category}
+                        </Button>
+                    )
+                }
+            </div>
         </aside>
     )
 }
